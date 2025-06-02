4057e68b3488e6016e8e193f3f6ee0fe8a365a0b
victornext <victorbrs517@gmail.com>
2025-06-02 16:17:21 -0300

    dependency

--- Model/Catergory.cs
+++ Model/Catergory.cs
@@ -1,6 +1,9 @@
-﻿namespace CatalogoAPI.Model
+﻿namespace CatalogoAPI.Model;
+
+public class Catergory
 {
-    public class Catergory
-    {
-    }
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? ImageUrl { get; set; }
+
 }
--- Model/Product.cs
+++ Model/Product.cs
@@ -1,6 +1,18 @@
-﻿namespace CatalogoAPI.Model
+﻿namespace CatalogoAPI.Model;
+
+public class Product
 {
-    public class Product
-    {
-    }
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Description { get; set; }
+
+    public Decimal Price { get; set; }
+
+    public string? ImageUrl { get; set; }
+
+    public float Stock { get; set; }
+
+    public DateTime DateOfRegistry { get; set; }
 }
